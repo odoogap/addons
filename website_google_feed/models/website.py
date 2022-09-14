e1bff5120643dfdbe6892470e7e1dd962c328087
Ahmed Talha <atalha@odoogap.com>
2022-09-14 11:58:35 +0100

    new branch 14.0

--- website_google_feed/models/website.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-
-class Website(models.Model):
-    _inherit = 'website'
-
-    google_feed_expire_time = fields.Integer(default=12)
