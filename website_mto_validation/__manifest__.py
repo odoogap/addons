94a2350b08d31a3807e3da4c764b29374f2a77a7
Diogo Duarte <dduarte@odoogap.com>
2022-09-15 12:18:34 +0100

    [FIX] change contraint validations

---
+++ website_mto_validation/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Website MTO Validation",
+    'version': '1.0',
+    'author': "ERPGAP",
+    'summary': 'Show warning if MTO and there is no BOM and seller and prevent publishing',
+    'description': 'Show warning if MTO and there is no BOM and seller and prevent publishing',
+    'website': "https://www.erpgap.com",
+    'depends': [
+        'sale',
+        'website_sale',
+        'stock',
+        'mrp',
+        'purchase'
+    ],
+    'data': [
+        'views/product_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
